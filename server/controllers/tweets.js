fd08da0d6d712bc8bcbc550077530499c0eabd1b
SophaktraLeng <sopheaktraleng4@gmail.com>
2024-10-13 11:05:54 +0700

    tweet CRUD

--- server/controllers/tweets.js
+++ server/controllers/tweets.js
@@ -13,4 +13,25 @@ const createTweet = (expressAsyncHandler (async(req,res)=>{
     const result = await newTweet.save()
     res.send(result)
 }))
-module.exports = createTweet
+const getAllTweet = (async(req,res)=>{
+    const tweets = await tweetModel.find({}).exec()
+    res.send(tweets)
+})
+const getTweetById = (async(req,res)=>{
+    const tweet = await tweetModel.findById(req.params.id)
+    res.send(tweet)
+})
+const updateById = expressAsyncHandler(async (req, res) => {
+    const { byUser, text, createdDate } = req.body;
+    const updatedTweet = await tweetModel.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    });
+    res.send(updatedTweet);
+});
+const deleteById = (expressAsyncHandler(async(req,res)=>{
+    const id = req.params.id
+    const tweet = await tweetModel.deleteOne({id})
+    res.send(tweet)
+}))
+
+module.exports = {createTweet, getAllTweet,getTweetById, updateById, deleteById}
--- server/routes/tweet.js
+++ server/routes/tweet.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const tweetRouter = express.Router();
-const createTweet = require('../controllers/tweets');
+const {createTweet, getAllTweet, getTweetById, deleteById,updateById } = require('../controllers/tweets');
 
 tweetRouter.post('/', createTweet)
-
+tweetRouter.get('/',getAllTweet)
+tweetRouter.get('/:id',getTweetById)
+tweetRouter.put('/:id',updateById)
+tweetRouter.delete('/:id',deleteById)
 
 module.exports = tweetRouter;
-4
