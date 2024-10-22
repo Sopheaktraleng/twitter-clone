4474af957cfe455168711c899dda1cec0a110352
SophaktraLeng <sopheaktraleng4@gmail.com>
2024-10-22 12:38:08 +0700

    responsive

--- client/src/components/Left_nav.vue
+++ client/src/components/Left_nav.vue
@@ -1,74 +1,81 @@
 <template>
-    <div class="p-2 h-screen top-0 w-min lg:w-64">
-        <div class="flex flex-col fixed px-3">
-            <router-link class="w-8 mx-auto lg:mx-0" to="/" >
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 24 24">
-                    <path d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z"></path>
-                </svg>
-            </router-link>
-          <div class="mt-5">
-            <router-link to="/home" class=" no-underline text-black">
-            <menu-item :title="'Home'" class="">
-              <home-icon class="w-6 mx-auto lg:mx-0"/>
-            </menu-item>
+  <div class="p-2 h-screen top-0 w-min lg:w-64 flex justify-start">
+      <div class="flex flex-col fixed lg:px-3">
+          <router-link class="w-8" to="/" >
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 24 24">
+                  <path d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z"></path>
+              </svg>
           </router-link>
-            <menu-item :title="'Explore'">
-              <explore-icon class="w-6 mx-auto lg:mx-0"/>
-            </menu-item>
-            <menu-item :title="'Notifications'">
-              <notification-icon class="w-6 mx-auto lg:mx-0"/>
-            </menu-item>
-            <menu-item :title="'Messages'">
-              <message-icon class="w-6 mx-auto lg:mx-0"/>
-            </menu-item>
-            <menu-item :title="'Lists'">
-              <list-icon class="w-6 mx-auto lg:mx-0"/>
-            </menu-item>
-            <menu-item :title="'Bookmarks'">
-              <bookmark-icon class="w-6 mx-auto lg:mx-0"/>
-            </menu-item>
-            <menu-item :title="'Communities'">
-              <communtities-icon class="w-6 mx-auto lg:mx-0"/>
-            </menu-item>
-            <menu-item :title="'Premium'">
-              <premium-icon class="w-6 mx-auto lg:mx-0"/>
-            </menu-item>
-            <router-link to="/profile" class=" no-underline text-black">
-              <menu-item :title="'Profile'">
-              <profile-icon class="w-6 mx-auto lg:mx-0" />
-            </menu-item></router-link>
-            
-
-            <menu-item :title="'More'">
-              <more-icon class="w-6 mx-auto lg:mx-0"/>
-            </menu-item>
+        <div class="mt-5">
+          <router-link to="/home" class=" no-underline text-black">
+          <menu-item :title="'Home'" class="flex justify-start">
+            <home-icon class="w-6 "/>
+          </menu-item>
+        </router-link>
+          <menu-item :title="'Explore'">
+            <explore-icon class="w-6"/>
+          </menu-item>
+          <menu-item :title="'Notifications'">
+            <notification-icon class="w-6"/>
+          </menu-item>
+          <menu-item :title="'Messages'">
+            <message-icon class="w-6"/>
+          </menu-item>
+          <menu-item :title="'Lists'">
+            <list-icon class="w-6"/>
+          </menu-item>
+          <menu-item :title="'Bookmarks'">
+            <bookmark-icon class="w-6"/>
+          </menu-item>
+          <menu-item :title="'Communities'">
+            <communtities-icon class="w-6"/>
+          </menu-item>
+          <menu-item :title="'Premium'">
+            <premium-icon class="w-6"/>
+          </menu-item>
+          <router-link to="/profile" class=" no-underline text-black">
+            <menu-item :title="'Profile'">
+            <profile-icon class="w-6" />
+          </menu-item></router-link>
           
-            <menu-item class=" w-48 h-12 text-xl rounded-full bg-blue justify-center font-sans font-bold text-white">Post
-            </menu-item>
-            </div>
-        
-        <!-- User Menu -->
-        <div 
 
-              class=" mt-5 w-14 mx-auto mb-5 rounded-full  p-2  font-sans pr-32 pb-32">
-              <div class="flex flex-row">
-                <img
-                  class="w-10 h-10 rounded-full"
-                  src="https://pbs.twimg.com/profile_images/1308769664240160770/AfgzWVE7_normal.jpg"
-                  alt="Joe Biden"
-                />
-                <div class=" flex-col ml-2">
-                  <h1 class="text-gray-800 dark:text-white font-bold text-sm">
-                    Logout
-                  </h1>
-                  <p class="text-gray-400 text-sm"></p>
-                </div>
+          <menu-item :title="'More'">
+            <more-icon class="w-6"/>
+          </menu-item>
+        
+          <menu-item class="w-8 rounded-full lg:bg-blue font-sans font-bold text-white lg:w-48 lg:h-12 lg:text-xl lg:justify-center lg:rounded-full">
+  <!-- Show icon for small screens -->
+  <svg class="block lg:hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512" xml:space="preserve">
+    <path fill-rule="evenodd" clip-rule="evenodd" fill="#00ACED" d="M512 107.867c0 7.031-2.203 13.527-5.977 18.836-10.062 15.051-23.727 25.977-37.367 37.422a15.803 15.803 0 0 0-4.805 10.531c-1.523 69.156-27.141 148.246-72.78 200.512-54.586 62.49-129 104.881-215.078 104.881-8.824 0-16-7.172-16-16 0-8.844 7.176-16 16-16 158.555 0 244.944-138.6 255.007-256.924.469-1.652.758-3.785.758-6.816 0-1.008.016-2.012.07-2.996.789-13.796 6.961-26.156 16.43-35.019.469-.449.828-.859 1.172-1.273 11.555-11.375 22.211-24.668 30.461-37.152-11.719 5.258-24.078 9.238-36.922 11.93-2.148 0-3.961-1.812-3.961-4a3.98 3.98 0 0 1 1.164-2.816c.031-.035-.109.016-.125.016 12.609-11.012 22.281-25.402 27.484-41.816-8.469 5.059-17.406 9.418-26.68 13.098-5.992 2.387-12.5 3.766-19.359 3.766-10.672 0-20.593-3.211-28.843-8.719-.32-.215-.648-.359-.969-.555-13.672-9.184-29.617-14.711-47.68-14.711l-1.883.34c-39.742 0-69.945 22.406-81.234 56.707-.317.147-3.551 14.406-3.875 16.453-2.996 19.121-19.535 33.742-39.504 33.742a37.59 37.59 0 0 1-6.387-.539c-52.472-9.812-99.847-32.504-135.207-69.406-.215-.18-.379-.234-.684-.539-4.68-4.684-12.27-4.684-16.969 0a12.307 12.307 0 0 0-2.531 3.73c-.141.344-.23.344-.34.414-4.27 9.867-7.391 20.539-7.391 31.09 0 21.832 8.879 41.761 22.227 57.601 0 .02-.16-.07-.09 0a8.005 8.005 0 0 1 1.867 5.148c0 4.414-3.59 8-8.004 8-.879 0-1.703-.23-2.492-.5a15.06 15.06 0 0 0-.375-.16c-.145.051-.305.125-.559 0a7.903 7.903 0 0 0-3.566-.844c-4.344 0-7.859 3.516-7.969 7.82 4.164 28.469 21.637 53.676 45.227 68.458-.305-.031-.484-.031-.375.031a8.019 8.019 0 0 1 3.621 6.695c0 4.414-3.586 8-8 8-.09 0-.18-.016-.27-.016-.105-.023-.195-.055-.305-.07-.285-.039-.555-.094-.844-.125-2.152.109-3.84 1.812-3.84 3.961a3.8 3.8 0 0 0 .395 1.742c6.512 16.219 17.527 30.078 31.395 40.219.164.125.199.234.395.375 1.184.844 2.297 1.812 3.375 2.875 10.922 10.922 10.922 28.648 0 39.594-3.355 3.328-7.34 5.664-11.555 6.953-.035.016-.016 0-.055.016-16.395 5.203-33.762 8.219-52.203 8.219h-9.992c-2.207 0-4 1.797-4 4 0 1.297.645 2.375 1.578 3.094 6.586 3.906 13.332 7.727 20.344 11.281l-.016.016c4.715 2.639 7.965 7.623 7.965 13.436 0 8.523-6.906 15.43-15.43 15.43-3.48 0-6.672-1.203-9.258-3.141-11.211-5.867-22.082-12.148-32.414-18.836-12.59-8.142-17.938-23.986-12.879-38.197 4.594-12.992 16.703-19.086 30.105-19.086 5.953.719 17.902 0 24 0 12.289 0 22.355-2.531 33.996-5.344-17.473-14.82-30.965-34.555-38.266-57.57a32.619 32.619 0 0 1-.645-17.398c-20.648-22.531-35.089-55.293-35.089-87.672.16-8.02 5.273-14.711 10.496-20.434-5.059-13.761-10.496-28.433-10.496-43.574 0-22.137 8.558-43.969 19.554-63.004 5.309-9.184 17.957-16.164 28.449-16.988 9.598 0 18.031 5.273 24.148 12.844 30.176 37.352 77.406 57.426 123.871 68.906.789.141 1.633.25 2.477.25 7.625 0 13.938-5.293 15.59-12.434C242.817 71.844 284.563 32 342.118 32c24.344 0 48.078 7.34 68.147 20.633 3.125 1.973 6.766 3.176 10.727 3.176 1.758 0 3.445-.289 5.039-.719 8.672-3.23 17.117-6.98 25.117-11.785 5.078-3.035 10.727-3.266 16.359-3.266 7.109 0 14.195 1.129 20.008 5.793 10.422 8.379 14.633 22.391 10.562 35.199 0 .035-.016.07-.016.105A32.493 32.493 0 0 1 512 107.867zM31.998 368.043c-1.258-.141 1.253 0 0 0zm32-311.844a16 16 0 1 1 32 0 16 16 0 1 1-32 0zm64 0a16 16 0 1 1 32 0 16 16 0 1 1-32 0zm32-48a16 16 0 1 1 32 0 16 16 0 1 1-32 0z"/>
+  </svg>
+
+  <!-- Show "Post" button for large screens -->
+  <span class="hidden lg:block">Post</span>
+</menu-item>
+
+          </div>
+      
+      <!-- User Menu -->
+      <div 
+
+            class=" mt-5 lg:mt-20 font-sans">
+            <div class="flex flex-row">
+              <img
+                class="w-10 h-10 rounded-full"
+                src="https://pbs.twimg.com/profile_images/1308769664240160770/AfgzWVE7_normal.jpg"
+                alt="Joe Biden"
+              />
+              <div class=" flex-col ml-2">
+                <h1 class="font-bold text-sm hidden lg:block">
+                  Logout
+                </h1>
               </div>
             </div>
-            <!-- /User Menu -->
-        </div>
+          </div>
+          <!-- /User Menu -->
       </div>
-  
+    </div>
+
 </template>
 <script>
 import BookmarkIcon from "./icons/BookmarkIcon.vue";
@@ -84,17 +91,17 @@ import PremiumIcon from "./icons/PremiumIcon.vue";
 import ProfileIcon from './icons/ProfileIcon.vue';
 
 export default{
-    name: "Left_nav",
-    // methods:{
-    //   ...mapActions(useAuthStore, ['logout']),
-    //   logUserOut(){
-    //     this.logout();
-    //     this.$router.push('/');
-    //   }
-    // },
-    // computed:{
-    //   ...mapState(useAuthStore, ['loggedUser'])
-    // },
-    components:{ HomeIcon, ListIcon, MessageIcon, PremiumIcon, NotificationIcon, MoreIcon, CommuntitiesIcon, BookmarkIcon,ExploreIcon, ProfileIcon, MenuItem}
+  name: "Left_nav",
+  // methods:{
+  //   ...mapActions(useAuthStore, ['logout']),
+  //   logUserOut(){
+  //     this.logout();
+  //     this.$router.push('/');
+  //   }
+  // },
+  // computed:{
+  //   ...mapState(useAuthStore, ['loggedUser'])
+  // },
+  components:{ HomeIcon, ListIcon, MessageIcon, PremiumIcon, NotificationIcon, MoreIcon, CommuntitiesIcon, BookmarkIcon,ExploreIcon, ProfileIcon, MenuItem}
 };
 </script>
