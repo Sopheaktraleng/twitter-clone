b9b1e92816cc834d379553c1cad66b908e103410
Leng Sopheaktra <sopheaktraleng4@gmail.com>
2025-04-10 15:42:58 +0700

    refactor: re-customize sign in form

--- client/src/views/SignIn.vue
+++ client/src/views/SignIn.vue
@@ -1,37 +1,51 @@
 <template>
-    <div class="h-full flex flex-col justify-center bg-black rounded-lg w-full">
-        <div class="mx-auto w-96 h-96 rounded-lg">
-            <router-link to="/" >
+    <div class=" bg-grey-lightest rounded-lg min-w-full p-10">
+        <div>
+          <button @click="$emit('close')" class="absolute top-3 right-4 text-2xl border-0 bg-grey-lightest">
+            &times;
+          </button>
+          <div class="flex flex-col justify-center items-center">
+            <router-link to="/" class="items-center justify-center">
                 <svg class=" p-3" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 24 24">
                     <path d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z"></path>
                 </svg>
             </router-link>
-                <div class="font-bold text-3xl items-center px-3">Sign in to X
+                <div class=" w-full flex flex-col justify-center items-center">
+                  <h1 class="font-bold text-black">Sign in to X</h1>
+                  <button-defualt class="flex justify-center gap-2 bg-white border border-black p-3">
+                    <google-icon class="px-2"></google-icon>
+                    Sign up with Google
+                  </button-defualt>
+                  <button-defualt class="flex justify-center gap-2 bg-white border border-black">
+                    <apple-icon class="px-2"></apple-icon>
+                    Sign up with Apple
+                  </button-defualt>
+                  <DeviderRule class="text-black"></DeviderRule>
 
-    <form @submit.prevent="handleLogin" class="w-96 space-y-2 mx-20">
-                    <div class=" mt-5">
-                        <div class="text-lg">Username:
-                            <input type="text" id="username" v-model="username"/>
-                        </div>
-                    </div>
-                    <div class=" mt-5">
-                        <div class="text-lg">Password:
-                        <input type="password" id="password" v-model="password"/>
-                        </div>
-                    </div>
-                    <button-defualt class="w-1/2 mt-5 mb-5 rounded-xl bg-blue text-white p-3" type="submit">Login</button-defualt>
+    <form @submit.prevent="handleLogin" class="flex flex-col justify-center items-center" >
+
+
+                            <input type="text" id="username" v-model="username" placeholder="Phone, Email, or Username" class=" w-64 h-8 my-2 bg-white"/>
+                        <input type="password" id="password" v-model="password" class=" w-64 h-8 bg-white" placeholder="Password..."/>
+
+
+                    <button-defualt class="rounded-xl bg-blue text-white p-3 border mt-4" type="submit">Login</button-defualt>
                 </form>
             </div>
             </div>
+    </div>
     </div>
 </template>
 <script>
 import { useUserStore } from "@/store/user";
 import ButtonDefualt from "@/components/icons/ButtonDefualt.vue";
 import {mapState, mapActions } from "pinia"
+import AppleIcon from "@/components/icons/AppleIcon.vue";
+import DeviderRule from "@/components/icons/Devider-Rule.vue";
+import GoogleIcon from "@/components/icons/GoogleIcon.vue";
 export default {
     name: "SignIn",
-    components:{ButtonDefualt},
+    components:{GoogleIcon, DeviderRule, AppleIcon, ButtonDefualt},
     data() {
         return {
             username: "",
