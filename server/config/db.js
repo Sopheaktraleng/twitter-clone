ca84c9965d52620a330123ee0e5e776ecad7fbc0
Leng Sopheaktra <sopheaktraleng4@gmail.com>
2025-04-11 15:08:21 +0700

    fix: resolve undefine database problem

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
-const mongoURL = process.env.MONGO_URL
+const mongoose = require("mongoose");
+require("dotenv").config();
+const mongoURI = process.env.MONGO_URI;
 
 async function dbConnect() {
-    mongoose.connection.on('connected', ()=>console.log('db is connected!'));
-    await mongoose.connect(mongoURL, {
-        dbName: 'twitterdb'
-    })
+    mongoose.connection.on("connected", () => console.log("db is connected!"));
+    await mongoose.connect(mongoURI, {
+        dbName: "twitterdb",
+    });
 }
-module.exports = dbConnect
+module.exports = dbConnect;
