7cb83357ece11bda7d012c7299b732e70352fb11
Leng Sopheaktra <sopheaktraleng4@gmail.com>
2025-04-22 14:53:05 +0700

    fix: resolve eslint config for vue3 compatibility

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,15 @@
+// .eslintrc.cjs
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    // your custom rules here
+  },
+}
